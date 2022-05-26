ed2047eb9dbe92c360d1d801041f8f2182595696
afurkanbicak <afurkanbicak@gmail.com>
2022-05-26 03:59:02 +0300

    genel search işlemi yapıldı

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,99 @@
-import React from 'react'
 import { CloseButton, Form } from 'react-bootstrap'
 
 const Search = ( { searchData, setData } ) => {
 
+    //? Türkçe karakterler ingilizce karakterlere çevrilir.
+    String.prototype.turkishtoEnglish = function () {
+        return this.replace('Ğ','g')
+            .replace('Ü','u')
+            .replace('Ş','s')
+            .replace('I','i')
+            .replace('İ','i')
+            .replace('Ö','o')
+            .replace('Ç','c')
+            .replace('ğ','g')
+            .replace('ü','u')
+            .replace('ş','s')
+            .replace('ı','i')
+            .replace('ö','o')
+            .replace('ç','c')
+    };
+    
+    //? Genel arama yapan fonksiyon.    
+    const generalSearch = (e) => {
+        let newArr = [];
+        searchData.forEach(element => {
+            for (let i in element){
+
+                //? string ifadeler karşılaştırıldı bitti.
+                if (element[i].toString().toLowerCase().includes(e.toString().toLowerCase())) {
+                
+                    if (!newArr.includes(element)){
+                        newArr.push(element)
+                    }
+                    setData(newArr)                                    
+                }
+                
+                //? obje ve diziler iç içe
+                if (typeof(element[i]) === 'object') {
+                    for (let [key, value] of Object.entries(element)) {
+    
+                        //? dizidir ama değilse de bu bir obje
+                        if (value instanceof Array) {
+                            
+                            //? dizi ama dizi içinde objelerde olabilir.
+                            value.forEach((item) => {
+                               
+                                //! Sadece [1,2,3] gibi diziler "stirng veya number" döner.
+                                //! Dizi içinde [{},{},{}] gibi ]objeler varsa "object" döner
+
+                                //? Sadece dizi [1,2,3] ise dizi elemanları tek tek gezer.
+                                if (typeof(item) === 'string' || typeof(item) === 'number') {
+                                    
+                                    if (item.toString().toLowerCase().includes(e.toString().toLowerCase())) {
+                
+                                        if (!newArr.includes(element)){
+                                            newArr.push(element)
+                                        }
+                                        setData(newArr)                                    
+                                    }
+                                } else {
+                                    //? Dizi içinde objeler [{},{},{}] ise tek tek obje elemanlarını gezer.
+                                    // console.log("Dizi+Objeler",item)
+                                    for (let i in item){
+                                        //console.log("NE",typeof(item[i]))
+                                        // if(item[i].toString().toLowerCase().includes(e.toString().toLowerCase())){
+                
+                                        //     if (!newArr.includes(item)){
+                                        //         newArr.push(item)
+                                        //     }
+                                        //     setData(newArr)                                    
+                                        // }
+                                    }
+                                }
+                            })
+                        } else {
+                            //? sadece objeler
+                            //console.log("SAdece OBJE",value)
+                            // for(let i in value){
+                            //     if(value[i].toString().toLowerCase().includes(e.toString().toLowerCase())){
+        
+                            //         if (!newArr.includes(value)){
+                            //             newArr.push(value)
+                            //         }
+                            //         setData(newArr)                                    
+                            //     }
+                            // }
+                        }
+                    }
+                }
+            }
+        });
+    }
+
     //? Ülke ismine göre arama yapan fonksiyon.
     const searchCountries = (e) => {
+        e = e.turkishtoEnglish();
         if(e === ''){
             return e
         } else {
@@ -16,49 +105,7 @@ const Search = ( { searchData, setData } ) => {
             setData(filterResult)
         }
     }
-
-    //? Ülke bölgesine göre arama yapan fonksiyon.
-    const searchRegion = (e) => {
-        if(e === ''){
-            return e
-        } else {
-            const filterResult = searchData.filter(item => {
-                return(
-                    item.region?.toLowerCase().includes(e.toLowerCase())
-                )
-            })
-            setData(filterResult)
-        }
-    }
     
-    //? Ülke başkentine göre arama yapan fonksiyon.
-    const searchCapital = (e) => {
-        if(e === ''){
-            return e
-        } else {
-            const filterResult = searchData.filter(item => {
-                return(
-                    item.capital?.toLowerCase().includes(e.toLowerCase())
-                )
-            })
-            setData(filterResult)
-        }
-    }
-
-     //? Ülke başkentine göre arama yapan fonksiyon.
-     const searchAlphaCode = (e) => {
-        if(e === ''){
-            return e
-        } else {
-            const filterResult = searchData.filter(item => {
-                return(
-                    item.alpha3Code?.toLowerCase().includes(e.toLowerCase())
-                )
-            })
-            setData(filterResult)
-        }
-    }
-
   return (
       
     <div className = 'shadow-lg p-3 mb-5 bg-white rounded m-4'>  
@@ -69,6 +116,7 @@ const Search = ( { searchData, setData } ) => {
             <div className = 'col-md-5'>
                 <Form.Label className = 'text-primary'> Countries </Form.Label>
                 <Form.Control 
+                    size        =   'lg'
                     type        =   'text' 
                     placeholder =   'Countries Search...'
                     onChange    =   { (e) => searchCountries(e.target.value) } 
@@ -76,31 +124,12 @@ const Search = ( { searchData, setData } ) => {
             </div>
 
             <div className = 'col-md-5'>
-                <Form.Label className='text-primary'>Capital</Form.Label>
+                <Form.Label className='text-primary'> General Search </Form.Label>
                 <Form.Control 
+                    size        =   'lg'
                     type        =   'text' 
-                    placeholder =   'Capital Search...'
-                    onChange    =   { (e) => searchCapital(e.target.value) } 
-                />
-            </div>
-        </div>
-
-        <div className = 'd-flex justify-content-center row mb-4 mt-2' style={{gap:'100px'}}>
-            <div className = 'col-md-5'>
-                <Form.Label className='text-primary'>Region</Form.Label>
-                <Form.Control 
-                    type        =   'text' 
-                    placeholder =   'Region Search...'
-                    onChange    =   { (e) => searchRegion(e.target.value) } 
-                />
-            </div>
-
-            <div className = 'col-md-5'>
-                <Form.Label className='text-primary'>Alpha3 Code</Form.Label>
-                <Form.Control  
-                    type        =   'text' 
-                    placeholder =   'Alpha3 Code Search...'
-                    onChange    =   { (e) => searchAlphaCode(e.target.value) } 
+                    placeholder =   'General Search...'
+                    onChange    =   { (e) => generalSearch(e.target.value) } 
                 />
             </div>
         </div>
