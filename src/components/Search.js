1a10595d05f29e72245c6f6de0b7aa3d488c5b63
afurkanbicak <afurkanbicak@gmail.com>
2022-05-26 11:12:30 +0300

    code review

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,31 +2,14 @@ import { CloseButton, Form } from 'react-bootstrap'
 
 const Search = ( { searchData, setData } ) => {
 
-    //? Türkçe karakterler ingilizce karakterlere çevrilir.
-    String.prototype.turkishtoEnglish = function () {
-        return this.replace('Ğ','g')
-            .replace('Ü','u')
-            .replace('Ş','s')
-            .replace('I','i')
-            .replace('İ','i')
-            .replace('Ö','o')
-            .replace('Ç','c')
-            .replace('ğ','g')
-            .replace('ü','u')
-            .replace('ş','s')
-            .replace('ı','i')
-            .replace('ö','o')
-            .replace('ç','c')
-    };
-    
-    //? Genel arama yapan fonksiyon.    
-    const generalSearch = (e) => {
+    function generalSearch(e) {
+        e = e.toLocaleLowerCase('tr');
         let newArr = [];
         searchData.forEach(element => {
-            for (let i in element){
-
+            for(let i in element){
+                //console.log(typeof(element[i]), element[i])
                 //? string ifadeler karşılaştırıldı bitti.
-                if (element[i].toString().toLowerCase().includes(e.toString().toLowerCase())) {
+                if(element[i].toString().toLowerCase().includes(e.toString().toLowerCase())){
                 
                     if (!newArr.includes(element)){
                         newArr.push(element)
@@ -35,30 +18,59 @@ const Search = ( { searchData, setData } ) => {
                 }
                 
                 //? obje ve diziler iç içe
-                if (typeof(element[i]) === 'object') {
-                    for (let [key, value] of Object.entries(element)) {
-    
+                if(typeof(element[i]) === 'object') {
+                    //console.log("obje", element[i])
+                    for(let [key, value] of Object.entries(element)){
+                        // console.log("value",value)
+
                         //? dizidir ama değilse de bu bir obje
-                        if (value instanceof Array) {
-                            
+                        if(value instanceof Array) {
+                            //console.log("Dizi", value)
                             //? dizi ama dizi içinde objelerde olabilir.
                             value.forEach((item) => {
-                               
+                                //console.log("typeOf", typeof(item), item)
                                 //! Sadece [1,2,3] gibi diziler "stirng veya number" döner.
                                 //! Dizi içinde [{},{},{}] gibi ]objeler varsa "object" döner
 
                                 //? Sadece dizi [1,2,3] ise dizi elemanları tek tek gezer.
-                                if (typeof(item) === 'string' || typeof(item) === 'number') {
-                                    
-                                    if (item.toString().toLowerCase().includes(e.toString().toLowerCase())) {
+                                if(typeof(item) === 'string' || typeof(item) === 'number'){
+                                    //console.log("Dizi", item)
+                                    if(item.toString().toLowerCase().includes(e.toString().toLowerCase())){
                 
                                         if (!newArr.includes(element)){
                                             newArr.push(element)
                                         }
                                         setData(newArr)                                    
                                     }
-                                } 
+                                } else{
+                                    //? Dizi içinde objeler [{},{},{}] ise tek tek obje elemanlarını gezer.
+                                    //console.log("Dizi+Objeler",item)
+                                    
+                                    for(let i in item){
+                                        //console.log("NE",typeof(item[i]))
+                                        if(item[i].toString().toLowerCase().includes(e.toString().toLowerCase())){
+                
+                                            if (!newArr.includes(element)){
+                                                newArr.push(element)
+                                            }
+                                            setData(newArr)                                    
+                                        }
+                                    }
+                                }
                             })
+                           
+                        } else {
+                            //? sadece objeler
+                            // console.log("SAdece OBJE",value)
+                            for(let i in value){
+                                if(value[i].toString().toLowerCase().includes(e.toString().toLowerCase())){
+        
+                                    if (!newArr.includes(element)){
+                                        newArr.push(element)
+                                    }
+                                    setData(newArr)                                    
+                                }
+                            }
                         }
                     }
                 }
@@ -66,9 +78,10 @@ const Search = ( { searchData, setData } ) => {
         });
     }
 
+
     //? Ülke ismine göre arama yapan fonksiyon.
     const searchCountries = (e) => {
-        e = e.turkishtoEnglish();
+        e = e.toLocaleLowerCase('tr');
         if(e === ''){
             return e
         } else {
